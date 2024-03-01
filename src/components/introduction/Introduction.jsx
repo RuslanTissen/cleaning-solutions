14e1d8bdb2cb352dec66668056b487f34d82f7dc
SFERAS <rthyssen73@gmail.com>
2024-03-01 18:05:54 +0100

    new start

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -5,10 +5,12 @@ function Introduction() {
 	return (
 		<div className='introduction'>
 			<div className="introduction__img">
-				{/* <h1>Cleaning Solutions</h1> */}
+				<h1>Cleaning Solutions</h1>
 			</div>
 			<div className="introduction__link-cont">
-				<h3></h3>
+				<h3>GET A FREE Cost Estimate.</h3>
+				<a href="">Contact now </a>
+				<h6>leave you name, e-mail, phone to reach you back</h6>
 			</div>
 		</div>
 	)
