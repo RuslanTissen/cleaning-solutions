07f80f9db05aaa8022335d4dfc97c7ec41eb709a
SFERAS <rthyssen73@gmail.com>
2024-03-05 23:38:11 +0100

    gallery is done

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import "./Start.scss"
 import Introduction from '../introduction/Introduction.jsx'
 import About from '../about/About.jsx'
+import Work from '../work/Work.jsx'
 
 function Start() {
 	return (
 		<div className='starts'>
 
-			<Introduction/>
+			<Introduction />
 
-			<About/>
+			<About />
+
+			<Work />
 
 		</div>
 	)
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "./Work.scss"
+import { FaArrowRight } from 'react-icons/fa'
+import work1 from "../../images/work1.jpg"
+import work2 from "../../images/work2.jpg"
+import work3 from "../../images/work3.jpg"
+// import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function Work() {
-  return (
-	 <div className='work'></div>
-  )
+	return (
+		<div className='work'>
+
+			<div className="work__text-box">
+				<h5>Recent services provided</h5>
+				<h2>We assisted our clients</h2>
+				<a>get a contact <FaArrowRight /></a>
+			</div>
+
+			<div className="work__galery-box">
+			<Carousel>
+                <div>
+                    <img src={work1} />
+                    {/* <p className="legend">Legend 1</p> */}
+                </div>
+                <div>
+                    <img src={work2}/>
+                    {/* <p className="legend">Legend 2</p> */}
+                </div>
+                <div>
+                    <img src={work3}/>
+                    {/* <p className="legend">Legend 3</p> */}
+                </div>
+            </Carousel>
+			</div>
+
+		</div>
+	)
 }
 
 export default Work
